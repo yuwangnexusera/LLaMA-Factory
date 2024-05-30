e1adef54a645dc4a4199ff1db9640fd89011c186
yuwangnexusera <yu.wang@nexusera.com>
2024-05-30 11:11:30 +0800

    drug ds mul_gpu

--- parse_ds/recheck_ds.py
+++ parse_ds/recheck_ds.py
@@ -8,11 +8,15 @@ from utils import ds_label_wrapper
 from sft_prompt import sft_unit_prompt
 # default-locations包含不绑定报告类型日期的其他点位，包含报告类型日期的将在后续@wy TODO 补充
 class AlignDataset:
-    def __init__(self, unit_name):
+
+    def __init__(self, unit_name, ds_num, save_path):
         self.unit_name = unit_name
+        self.ds_num = ds_num
+        self.save_path = save_path
         self.ds = []
         self.unit_ds = []
         self.load_ds()
+
     def load_ds(self):
         ds_objs = ds_label_wrapper.select_ds()
         for ds in ds_objs:
@@ -37,16 +41,24 @@ class AlignDataset:
         for data in self.ds:
             output = data["result"]
             target_unit_data = output.get(self.unit_name, None)
-            if target_unit_data and isinstance(target_unit_data, dict):
-                target_unit_data = [target_unit_data]
+            if target_unit_data is not None:
+                if isinstance(target_unit_data, dict):
+                    target_unit_data = [target_unit_data]
+            else:
+                continue
             for index, unit_data in enumerate(target_unit_data):
                 res = self.fill_na_by_locs(unit_data)
                 self.unit_ds.append({"instruction":sft_unit_prompt.get(self.unit_name,""),"input": data["ocr"], "output": json.dumps(res, ensure_ascii=False)})
         return self.unit_ds
 
+    def save(self):
+        pd.DataFrame(self.unit_ds).to_json(self.save_path, orient="records",force_ascii=False, lines=False)
 
 if __name__=='__main__':
 
-
-    alignor = AlignDataset("基本信息")
-    print("@syu:", alignor.extract_specified_values())
+    unit = "治疗用药方案"
+    en_unit = "drug_treatment_plan"
+    alignor = AlignDataset(unit, 100, f"data/{en_unit}_recheck_zh.json")
+    res = alignor.extract_specified_values()
+    alignor.save()
+    print("@syu:")
