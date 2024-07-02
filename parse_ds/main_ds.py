a07db461c7b2b00ba678fa1c085c00c0019bf26f
yuwangnexusera <yu.wang@nexusera.com>
2024-07-02 13:22:30 +0800

    dataset-07-02

--- parse_ds/main_ds.py
+++ parse_ds/main_ds.py
@@ -6,14 +6,14 @@ from langchain_community.chat_models.openai import ChatOpenAI
 with open("utils/mapping_answer_zh_en.json", "r", encoding="utf-8") as f:
     mapping = json.load(f)
 # 治疗用药方案,基本信息,疾病,体征数据,诊断,影像学,基因检测,免疫检测,肿瘤治疗,治疗用药方案,合并疾病,   日期
-# TODO 单独处理日期
+# TODO 将新数据集补充进来，然后把ipynb里面的方法抽取到命令行中
 unit = ["病理","治疗用药方案","基本信息","疾病","体征数据","诊断","影像学","基因检测","免疫检测","肿瘤治疗","治疗用药方案","合并疾病"]
 for u in unit:
     en_unit = mapping.get(u)
     test_path = f"data/{en_unit}/test_zh.json"
     all_path = f"data/{en_unit}/all_zh.json"
-    sft_path = f"data/{en_unit}/sft_zh.json"
-    dpo_path = f"data/{en_unit}/dpo_zh.json"
+    # sft_path = f"data/{en_unit}/sft_zh.json"
+    # dpo_path = f"data/{en_unit}/dpo_zh.json"
     alignor = AlignDataset(u,"test")
     unit_res = alignor.unit_values()
     alignor.save(test_path, unit_res)
@@ -32,4 +32,3 @@ for u in unit:
 # TODO 记得去掉-NA，患者是否进行基因检测 NA->否
 
 print("@syu:")
-
