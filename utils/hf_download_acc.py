927b44e1b7233f6c4400284d04f4863ec4092857
root <yu.wang@nexusera.com>
2024-07-03 18:40:50 -0700

    4090

--- utils/hf_download_acc.py
+++ utils/hf_download_acc.py
@@ -4,10 +4,10 @@ os.system("pip install -U huggingface_hub hf_transfer")
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 # 设置保存的路径
-local_dir = "../models"
+local_dir = "/mnt/windows/Users/Admin/LLM/models"
 
 # 设置仓库id
-model_id = "shenzhi-wang/Llama3-8B-Chinese-Chat"
+model_id = "shenzhi-wang/Gemma-2-9B-Chinese-Chat"
 
 cmd = f"huggingface-cli download --resume-download {model_id} --local-dir {local_dir}/{model_id} --local-dir-use-symlinks False"
 
