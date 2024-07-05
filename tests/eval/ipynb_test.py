73c9877ee5ac8853f974fe331fd7efd844119a0c
Yu Wang <yuwang@nexusera.com>
2024-07-04 23:17:57 -0700

    07-05

--- tests/eval/ipynb_test.py
+++ tests/eval/ipynb_test.py
@@ -9,24 +9,24 @@ from parse_ds.sft_prompt import sft_unit_prompt
 print("*****************运行评估测试************************")
 args = dict(
     do_sample=True,  # False之后常被截断
-    model_name_or_path="/mnt/windows/Users/Admin/LLM/models/shenzhi-wang/Llama3-8B-Chinese-Chat",
-    adapter_name_or_path="/mnt/windows/Users/Admin/LLM/models/shenzhi-wang/nexusera_lora_extract",  # 加载之前保存的 LoRA 适配器
-    template="llama3",  # 和训练保持一致
+    model_name_or_path="/mnt/windows/Users/Admin/LLM/models/shenzhi-wang/Gemma-2-9B-Chinese-Chat",
+    adapter_name_or_path="/mnt/windows/Users/Admin/LLM/models/shenzhi-wang/nexusera_lora_extract_gemma",  # 加载之前保存的 LoRA 适配器
+    template="gemma",  # 和训练保持一致
     finetuning_type="lora",  # 和训练保持一致
     # quantization_bit=4,                    # 加载 4 比特量化模型
    # use_unsloth=True,  # 使用 UnslothAI 的 LoRA 优化来获得两倍的推理速度
     temperature=0.5,
     top_p=0.7,
     max_new_tokens=1024,
-    repetition_penalty=1.1,
-    length_penalty=1.1,
+    repetition_penalty=1.0,
+    length_penalty=1.0,
 )
 torch_gc()
 chat_model = ChatModel(args)
 f1_cal = F1score()
 # 测试推理
 
-unit_name = "肿瘤治疗"
+unit_name = "治疗用药方案"
 test_path_mapping = {
     "病理": "data/Pathology/test_zh.json",
     "治疗用药方案": "data/Treatment Drug Plan/test_zh.json",
@@ -98,13 +98,13 @@ with open(test_path_mapping.get(unit_name), "r", encoding="utf-8") as file:
     P = res_eval_metrics["precision"]
     R = res_eval_metrics["recall"]
     print(f"评估结果：{res_eval_metrics},F1:{2*P*R/(P+R)}")
-    with open(f"../res_eval_{unit_name}{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w", encoding="utf-8") as file:
-        file.write("评估结果：\n")
-        for key, value in res_eval_metrics.items():
-            file.write(f"{key}: {value}\n")
+    # with open(f"../res_eval_{unit_name}{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w", encoding="utf-8") as file:
+    #     file.write("评估结果：\n")
+    #     for key, value in res_eval_metrics.items():
+    #         file.write(f"{key}: {value}\n")
 
-        # 计算并写入 F1 值
-        F1 = 2 * P * R / (P + R)
-        file.write(f"F1: {F1}\n")
-    with open(f"../res_cmp_{unit_name}{datetime.now().strftime('%Y%m%d%H%M%S')}.json", "w", encoding="utf-8") as file:
-        json.dump(res_cmp, file, indent=4, ensure_ascii=False)
+    #     # 计算并写入 F1 值
+    #     F1 = 2 * P * R / (P + R)
+    #     file.write(f"F1: {F1}\n")
+    # with open(f"../res_cmp_{unit_name}{datetime.now().strftime('%Y%m%d%H%M%S')}.json", "w", encoding="utf-8") as file:
+    #     json.dump(res_cmp, file, indent=4, ensure_ascii=False)
--- utils/model_scope_down.py
+++ utils/model_scope_down.py
@@ -1,4 +1,4 @@
 # 从modelscope下载
 
 from modelscope import snapshot_download
-model_dir = snapshot_download("qwen/Qwen1.5-14B-Chat-GPTQ-Int8", cache_dir="/mnt/windows/Users/Admin/LLM/models")
+model_dir = snapshot_download("mixtral-8x7b-instruct-v0.1.Q3_K_M.gguf/mixtral-8x7b-instruct-v0.1.Q3_K_M.gguf", cache_dir="/mnt/windows/Users/Admin/LLM/models")
