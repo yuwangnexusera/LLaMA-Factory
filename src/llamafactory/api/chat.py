ef9b4463d2783c01766e461685cddc0a01f9962e
marko1616 <45327989+marko1616@users.noreply.github.com>
2024-08-22 12:24:34 +0800

    Update chat.py

--- src/llamafactory/api/chat.py
+++ src/llamafactory/api/chat.py
@@ -18,7 +18,7 @@ import json
 import os
 import re
 import uuid
-import numpy
+import numpy as np
 from typing import TYPE_CHECKING, AsyncGenerator, Dict, List, Optional, Tuple
 
 from ..data import Role as DataRole
