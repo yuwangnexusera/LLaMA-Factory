02cfa6c2b37f912b003ea8957885e55a32de7d69
marko1616 <45327989+marko1616@users.noreply.github.com>
2024-08-22 12:14:34 +0800

    Update chat.py

--- src/llamafactory/api/chat.py
+++ src/llamafactory/api/chat.py
@@ -18,6 +18,7 @@ import json
 import os
 import re
 import uuid
+import numpy
 from typing import TYPE_CHECKING, AsyncGenerator, Dict, List, Optional, Tuple
 
 from ..data import Role as DataRole
@@ -112,7 +113,7 @@ def _process_request(
                     else:  # web uri
                         image_stream = requests.get(image_url, stream=True).raw
 
-                    image = Image.open(image_stream).convert("RGB")
+                    image = numpy.array(Image.open(image_path).convert("RGB"))
         else:
             input_messages.append({"role": ROLE_MAPPING[message.role], "content": message.content})
 
