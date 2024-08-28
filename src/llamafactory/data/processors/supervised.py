84188d5b98d52bb3384fc4d88feb56ed2583e7be
simonJJJ <821898965@qq.com>
2024-08-28 16:51:35 +0800

    initial-commit

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -21,6 +21,7 @@ from .processor_utils import greedy_knapsack, infer_seqlen
 
 
 if TYPE_CHECKING:
+    from PIL.Image import Image as ImageObject
     from transformers import PreTrainedTokenizer, ProcessorMixin
 
     from ...hparams import DataArguments
@@ -39,6 +40,7 @@ def _encode_supervised_example(
     images: Sequence["ImageInput"],
     videos: Sequence["VideoInput"],
     template: "Template",
+    images: Sequence["ImageObject"],
     tokenizer: "PreTrainedTokenizer",
     processor: Optional["ProcessorMixin"],
     cutoff_len: int,
