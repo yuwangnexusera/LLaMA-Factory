e295eb0bff022fd761bc2f694304163d50a9a714
Yu Wang <yuwang@nexusera.com>
2024-11-07 15:45:32 +0800

    dc.yml

---
+++ test.sh
@@ -0,0 +1,11 @@
+curl https://app.nexusera.cn/lc2/series/extract -H 'Content-Type: application/json' -d '{
+  "llm": "gpt",
+  "llm_category": "gpt",
+  "cached": false,
+  "user_id": "T01107",
+  "series": "2",
+  "disease_type": "",
+  "images": [
+    "https://yoola1-bucket.oss-cn-zhangjiakou.aliyuncs.com/ocr/ssP022/2.jpeg/keep"
+  ]
+}'
