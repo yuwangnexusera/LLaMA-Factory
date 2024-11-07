4080060c9188f6dab4aadb145eb386d82f723d22
Yu Wang <yuwang@nexusera.com>
2024-11-07 11:19:17 +0800

    check_point

--- test_code.py
+++ test_code.py
@@ -1,36 +1,8 @@
-import sys
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
+import json
+import os
 
-# 将克隆的 ESRGAN 路径添加到 Python 路径
-sys.path.append("/path/to/ESRGAN")
-
-from RRDBNet_arch import RRDBNet
-from utils import load_pretrained_model
-
-# 定义 ESRGAN 模型
-model = RRDBNet(3, 3, 64, 23, gc=32)
-model_path = "/path/to/ESRGAN/experiments/pretrained_models/RRDB_ESRGAN_x4.pth"  # 更新为你的模型路径
-
-# 加载预训练模型
-model = load_pretrained_model(model, model_path)
-model.eval()
-
-# 打开图像
-image_path = "/root/LLM/data/a6d6c5854c9841468d743fa4088fe477.jpg"
-img = Image.open(image_path).convert("RGB")
-
-# 图像预处理
-preprocess = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]  # 将图像转为张量  # 标准化
-)
-input_tensor = preprocess(img).unsqueeze(0)
-
-# 使用模型进行超分辨率处理
-with torch.no_grad():
-    output_tensor = model(input_tensor)
-
-# 后处理并保存结果
-output_image = transforms.ToPILImage()(output_tensor.squeeze(0).cpu())
-output_image.save("/root/LLM/data/enhanced_image.jpg")
+if __name__ == "__main__":
+    string = """"""
+    # 清楚控制台输出 clear
+    os.system("clear")
+    print(json.dumps({"report": string},ensure_ascii=False))
--- tests/eval/ss/ss_gene_con.py
+++ tests/eval/ss/ss_gene_con.py
@@ -12,9 +12,9 @@ print("*****************运行评估测试************************")
 # internlm模型更好
 args = dict(
     do_sample=True,
-    model_name_or_path="/mnt/windows/Users/Admin/LLM/models/Shanghai_AI_Laboratory/internlm2_5-7b-chat",
-    adapter_name_or_path="/mnt/windows/Users/Admin/LLM/models/Shanghai_AI_Laboratory/susu_internlm2_5_v1/",  # 加载之前保存的 LoRA 适配器
-    template="intern2",  # 和训练保持一致
+    model_name_or_path="models/Qwen/Qwen2___5-7B-Instruct",
+    adapter_name_or_path="models/Qwen/reward",  # 加载之前保存的 LoRA 适配器
+    template="qwen",  # 和训练保持一致
     finetuning_type="lora",  # 和训练保持一致
     # quantization_bit=4,
     temperature=0.3,
@@ -27,41 +27,10 @@ if __name__ == "__main__":
     torch_gc()
     chat_model = ChatModel(args)
     ret_data = []
-    ori_data = pd.read_excel("/mnt/windows/wy/data/ss/test_data_ori.xlsx")
-    report_type_units = {
-            "出入院记录": 
-            "门诊病历"
-            "检查记录"
-            "基因检测"
-            "病理报告"
-            "病理会诊记录"
-            "其他疾病诊断书"
-            "出入院疾病诊断书"
-            "门诊疾病诊断书"
-            "疾病诊断书"
-            "病理会诊"
-        }
-    for index,row in ori_data.iterrows():
-        url = row["url"]
-        ocr_res = row["ocr_res"]
-        # 分类
-        category = sft_prompt.report_type_prompt.replace("{report}",ocr_res)
-        if category not in report_type_units:
-            continue
-        prompt_1 = sft_prompt.gene_prompt_step1.replace("{category}", category).replace("{url}", url)
-        print(prompt_1)
-        messages = []
-        messages.append({"role": "user", "content": prompt_1})
-        response_1 = ""
-        response_1 = chat_model.chat(messages)
-        response_1 = response_1[0].response_text
-        print(response_1)
-        # 第二步prompt
-        messages_2 = []
-        prompt_2 = sft_prompt.gene_prompt_step2.replace("{list_in_values}", response_1)
 
-        messages_2.append({"role": "user", "content": prompt_2})
-        response_2 = ""
-        response_2 = chat_model.chat(messages_2)
-        response_2 = response_2[0].response_text
-        print(response_2)
+    messages = []
+    messages.append({"role": "user", "content": "哈哈哈"})
+    response_1 = ""
+    response_1 = chat_model.chat(messages)
+    response_1 = response_1[0].response_text
+    print(response_1)
