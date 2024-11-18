470964de28b9e137e042d002bab1e139279b17f6
Yu Wang <yuwang@nexusera.com>
2024-11-18 17:38:43 +0800

    1118 data

--- utils/download/model_scope_down.py
+++ utils/download/model_scope_down.py
@@ -2,7 +2,7 @@
 
 from modelscope import snapshot_download
 model_dir = snapshot_download(
-    "stepfun-ai/GOT-OCR2_0",
-    cache_dir="/mnt/windows/Users/Admin/LLM/models/ocr/",
-    ignore_file_pattern="consolidated.safetensors"
+    "wwyuuuu/Qwen2.5-SFT",
+    cache_dir="../wwyuuuu/",
+    ignore_file_pattern="consolidated.safetensors",
 )
